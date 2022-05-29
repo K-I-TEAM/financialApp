aaa602a9815d045344c2479cab4d834125f72d14
Isai Albarran <isaialbarran@gmail.com>
2022-05-29 21:25:03 +0200

    connection with database established

--- back_end/dist/index.js
+++ back_end/dist/index.js
@@ -5,9 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const sequelize_1 = require("sequelize");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3001;
+const sequelize = new sequelize_1.Sequelize('db', 'postgres', 'postgres', {
+    host: 'localhost',
+    dialect: 'postgres',
+});
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    }
+    catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server!');
 });
--- back_end/index.ts
+++ back_end/index.ts
@@ -1,10 +1,25 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
+
+const sequelize = new Sequelize('db', 'postgres', 'postgres', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server!');
