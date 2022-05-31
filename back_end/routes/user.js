74ac995ed933b2c235f407fd57cb6025f1ef872c
Isai Albarran <isaialbarran@gmail.com>
2022-05-31 09:47:03 +0200

    add new endpoints

--- back_end/routes/user.js
+++ back_end/routes/user.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { createUser, listUsers } from '../controllers/user.js';
+import { createUser, getUser, listUsers, updateUser, deleteUser } from '../controllers/user.js';
 
 const router = Router();
 
 router.get('/users', listUsers);
 router.post('/users', createUser);
-router.put('/users/:id');
-router.delete('/users:id');
-router.get('/users/:id');
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
+router.get('/users/:id', getUser);
 
 export default router;
