6ad1e2c639c443d3947f2742a14d596e5d134d75
Isai Albarran <isaialbarran@gmail.com>
2022-06-03 09:29:07 +0200

    add transactions endpoints

--- back_end/app.js
+++ back_end/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import userRoutes from './routes/user.js';
+import transactionRoutes from './routes/transaction.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(userRoutes);
+app.use(transactionRoutes);
 
 export default app;
