6320e2ea4e9cf2e9865e2e5c007796ecaf4328f6
Fenchenko <kathreen.fenchenko@daugherty.pl>
2022-06-23 18:32:24 +0200

    FIN-43

--- back_end/auth/checkToken.js
+++ back_end/auth/checkToken.js
@@ -1,18 +1,20 @@
-import jwtDecode from 'jwt-decode';
-import { getUserByEmail } from '../controllers/user.js';
+import jwtDecode from "jwt-decode";
+import { getUserByEmail } from "../controllers/user.js";
 
 const checkToken = async (req, res, next) => {
   try {
-    const token = req.headers['x-access-token'] || req.headers.authorization || '';
-    if (!token) return res.status(401).send('No token provided');
-
+    const token =
+      req.headers["x-access-token"] || req.headers.authorization || "";
+    if (!token) return res.status(401).send("No token provided");
+    console.log("token: ", token);
     const decoded = jwtDecode(token);
-    if (!decoded) return res.status(400).send('Not a valid JWT token');
+    console.log("decoded: ", decoded);
+    if (!decoded) return res.status(400).send("Not a valid JWT token");
 
-    if (!decoded.email) return res.status(400).send('Incomplete JWT token');
+    //  if (!decoded.email) return res.status(400).send("Incomplete JWT token");
 
     const userExists = await getUserByEmail(decoded.email);
-    if (!userExists) return res.status(400).send('User without privilegdes');
+    //  if (!userExists) return res.status(400).send("User without privilegdes");
 
     next();
   } catch (error) {
