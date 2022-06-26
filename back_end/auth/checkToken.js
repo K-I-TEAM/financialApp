ca4517d19cbf7ec8b123ddc42873eea146162c8d
Isai Albarran <isaialbarran@gmail.com>
2022-06-26 14:03:35 +0200

    error messages

--- back_end/auth/checkToken.js
+++ back_end/auth/checkToken.js
@@ -1,10 +1,11 @@
 import { CognitoJwtVerifier } from 'aws-jwt-verify';
 import { cleanToken } from '../helper/balance.js';
+import { Error } from '../helper/errorMessages.js';
 
 const checkToken = async (req, res, next) => {
   try {
     let token = req.headers['x-access-token'] || req.headers.authorization || '';
-    if (!token) return res.status(401).send('No token provided');
+    if (!token) return res.status(401).send(Error.NO_TOKEN);
 
     // Verifier that expects valid access tokens:
     const verifier = CognitoJwtVerifier.create({
@@ -16,11 +17,10 @@ const checkToken = async (req, res, next) => {
     try {
       const payload = await verifier.verify(cleanToken(token));
       console.log('Token is valid. Payload:', payload);
+      next();
     } catch {
-      console.log('Token not valid!');
+      res.status(401).send(Error.NOT_VALID_TOKEN);
     }
-
-    next();
   } catch (error) {
     res.status(500).send(error);
   }
---
+++ back_end/helper/errorMessages.js
@@ -0,0 +1,6 @@
+const Error = {
+  NO_TOKEN: 'No token provided',
+  NOT_VALID_TOKEN: 'Token not valid',
+};
+
+export { Error };
