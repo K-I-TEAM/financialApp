4fbc636645cfa2357e86e7f38af15e4af9f542b0
kathleen-fen <57800272+kathleen-fen@users.noreply.github.com>
2022-07-17 21:46:19 +0200

    FIN-56 error page (#35)
    Co-authored-by: Fenchenko <kathreen.fenchenko@daugherty.pl>

--- front_end/src/components/NoMatch.tsx
+++ front_end/src/components/NoMatch.tsx
@@ -1,5 +1,21 @@
+import { Box, fontSize } from "@mui/system";
 import React from "react";
 
-const NoMatch: React.FC = () => <h2>NoMatch component</h2>;
+const NoMatch: React.FC = () => (
+  <Box
+    height="100vh"
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    flexDirection="column"
+  >
+    <Box textAlign="center" fontSize="72px">
+      404
+    </Box>
+    <Box textAlign="center" fontSize="20px">
+      OOPS! PAGE NOT FOUND!
+    </Box>
+  </Box>
+);
 
 export default NoMatch;
