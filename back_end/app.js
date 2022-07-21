d68d0f84816163c6b3a81a40b214b717d01cee19
Isai Albarran <isaialbarran@gmail.com>
2022-07-21 10:30:24 +0200

    add error functions

--- back_end/app.js
+++ back_end/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
+
 import userRoutes from './routes/user.js';
 import transactionRoutes from './routes/transaction.js';
 import categoryRoutes from './routes/category.js';
+
 import apiLayerRoutes from './routes/apiLayer.js';
-import cors from 'cors';
+
 import { checkToken } from './auth/checkToken.js';
+
+import cors from 'cors';
 
 const corsOptions = {
   origin: '*',
@@ -13,6 +17,24 @@ const corsOptions = {
 };
 
 const app = express();
+
+// Error handling Middleware functions
+const errorLogger = (error, request, response, next) => {
+  console.log(`error ${error.message}`);
+  next(error); // calling next middleware
+};
+
+const errorResponder = (error, request, response, next) => {
+  response.header('Content-Type', 'application/json');
+
+  const status = error.status || 400;
+  response.status(status).send(error.message);
+};
+
+const invalidPathHandler = (request, response, next) => {
+  response.status(400);
+  response.send(`Invalid path, ${request.url} does not exists`);
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -23,5 +45,9 @@ app.use(userRoutes);
 app.use(transactionRoutes);
 app.use(categoryRoutes);
 app.use(apiLayerRoutes);
+
+app.use(errorLogger);
+app.use(errorResponder);
+app.use(invalidPathHandler);
 
 export default app;
