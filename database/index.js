bd91b09d13b674cf58d29587dc04dd51889b7f8e
Isai Albarran <isaialbarran@gmail.com>
2022-07-29 12:03:40 +0200

    add production db

--- database/index.js
+++ database/index.js
@@ -3,14 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-  },
-  logging: false,
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 export { sequelize };
