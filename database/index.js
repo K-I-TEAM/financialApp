dc8b4a3bb6231310defb555d57f69f12c6c7b1f3
Isai Albarran <isaialbarran@gmail.com>
2022-07-29 12:31:45 +0200

    change sequelize config

--- database/index.js
+++ database/index.js
@@ -3,6 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+let sequelize;
+
+if (!process.env.ENVIRONMET) {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+    },
+    logging: false,
+  });
+} else {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
+}
 
 export { sequelize };
